0a90ef24587b2c0bb3234eb222dab467557079ad
Myon <myon.cn@gmail.com>
2014-05-31 01:38:45 +0800

    果然gm中文就是问题多

---
+++ tiebaLastPage/tiebaLastPage.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        tiebaLastPage 
+// @namespace   Myon
+// @description 贴吧列表加入直达最后一页的链接
+// @include     http://tieba.baidu.com/f?kw=*
+// @author      Myon<myon.cn@gmail.com>
+// @downloadURL https://github.com/iMyon/gm_scripts/raw/master/tiebaLastPage/tiebaLastPage.user.js
+// @updateURL   https://github.com/iMyon/gm_scripts/raw/master/tiebaLastPage/tiebaLastPage.meta.js
+// @icon        http://tb.himg.baidu.com/sys/portrait/item/c339b7e2d3a1b5c4c3a8d726
+// @version     1.0.2
+// ==/UserScript==
