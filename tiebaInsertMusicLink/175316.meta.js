6eeb5ecd7348d88e660a90beb9b93c1d0154ae06
Myon <myon.cn@gmail.com>
2014-08-29 14:25:20 +0800

    兼容gm2.x jq

--- tiebaInsertMusicLink/175316.meta.js
+++ tiebaInsertMusicLink/175316.meta.js
@@ -4,9 +4,10 @@
 // @grant       GM_addStyle
 // @description 贴吧插入自定义音乐链接
 // @include     http://tieba.baidu.com/*
+// @require     http://libs.baidu.com/jquery/1.9.0/jquery.js
 // @author      Myon<myon.cn@gmail.com>
 // @downloadURL https://github.com/iMyon/gm_scripts/raw/master/tiebaInsertMusicLink/175316.user.js
 // @updateURL   https://github.com/iMyon/gm_scripts/raw/master/tiebaInsertMusicLink/175316.meta.js
 // @icon        http://tb.himg.baidu.com/sys/portrait/item/c339b7e2d3a1b5c4c3a8d726
-// @version     1.0.1
+// @version     1.0.2
 // ==/UserScript==
--- tiebaInsertMusicLink/README.md
+++ tiebaInsertMusicLink/README.md
@@ -1,3 +1,5 @@
 #简介
 
+[脚本安装](https://github.com/iMyon/gm_scripts/raw/master/tiebaInsertMusicLink/175316.user.js)
+
 ![](http://imgsrc.baidu.com/forum/pic/item/e6a099ef76c6a7efd529fbadfffaaf51f2de665a.png)
