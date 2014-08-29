6b834c2059969bbb21d3964045de076e61d3edd0
Myon <myon.cn@gmail.com>
2014-08-29 13:04:06 +0800

    gm2 jq兼容

--- 贴吧名片还原旧版/180350.user.js
+++ 贴吧名片还原旧版/180350.user.js
@@ -3,18 +3,17 @@
 // @namespace   Myon
 // @grant       GM_addStyle
 // @description 还原贴吧名片到旧版名片
+// @require     http://libs.baidu.com/jquery/1.9.0/jquery.js
 // @include     http://tieba.baidu.com/*
 // @include     http://tieba.baidu.com/f*ct=*
 // @author      Myon<myon.cn@gmail.com>
 // @downloadURL https://github.com/iMyon/gm_scripts/raw/master/%E8%B4%B4%E5%90%A7%E5%90%8D%E7%89%87%E8%BF%98%E5%8E%9F%E6%97%A7%E7%89%88/180350.user.js
 // @updateURL   https://github.com/iMyon/gm_scripts/raw/master/%E8%B4%B4%E5%90%A7%E5%90%8D%E7%89%87%E8%BF%98%E5%8E%9F%E6%97%A7%E7%89%88/180350.meta.js
 // @icon        http://tb.himg.baidu.com/sys/portrait/item/c339b7e2d3a1b5c4c3a8d726
-// @version     1.1.2
+// @version     1.1.3
 // ==/UserScript==
 
-var $ = unsafeWindow.$;
 var l_panelHtml;
-
 
 addNodeInsertedListener('.j_user_card,a.at',function(){
   $(this).hover(function() {
