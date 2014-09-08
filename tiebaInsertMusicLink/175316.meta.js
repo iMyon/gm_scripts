c822ac394f3739e601374f07b0fb49a6bb3d4bac
Myon <myon.cn@gmail.com>
2014-09-08 11:18:19 +0800

    失效脚本

--- tiebaInsertMusicLink/175316.meta.js
+++ tiebaInsertMusicLink/175316.meta.js
@@ -2,12 +2,12 @@
 // @name        tiebaInsertMusicLink 
 // @namespace   Myon
 // @grant       GM_addStyle
-// @description 贴吧插入自定义音乐链接
+// @description 警告：脚本在高版本火狐导致无法发帖，暂时没空研究度娘了，空白更新
 // @include     http://tieba.baidu.com/*
 // @require     http://libs.baidu.com/jquery/1.9.0/jquery.js
 // @author      Myon<myon.cn@gmail.com>
 // @downloadURL https://github.com/iMyon/gm_scripts/raw/master/tiebaInsertMusicLink/175316.user.js
 // @updateURL   https://github.com/iMyon/gm_scripts/raw/master/tiebaInsertMusicLink/175316.meta.js
 // @icon        http://tb.himg.baidu.com/sys/portrait/item/c339b7e2d3a1b5c4c3a8d726
-// @version     1.0.2
+// @version     1.0.3
 // ==/UserScript==
