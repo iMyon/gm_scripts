fcadc8cbff434f126e7ed8fdba4c23c262b08dcc
Myon <myon.cn@gmail.com>
2019-09-07 18:17:58 +0800

    添加贴吧图片上传脚本

--- tiebaUploadImagePlus/tiebaUploadImagePlus.user.js
+++ tiebaUploadImagePlus/tiebaUploadImagePlus.user.js
@@ -2,12 +2,12 @@
 // @name        贴吧图片上传加强
 // @namespace   Myon
 // @grant       none
-// @description 贴吧图片上传加强
+// @description 添加图片粘贴和拖拽上传
 // @include     http://tieba.baidu.com/*
 // @include     https://tieba.baidu.com/*
 // @author      Myon<myon.cn@gmail.com>
-// @downloadURL https://github.com/iMyon/gm_scripts/raw/master/tiebaPasteImage/tiebaPasteImage.user.js
-// @updateURL   https://github.com/iMyon/gm_scripts/raw/master/tiebaPasteImage/tiebaPasteImage.meta.js
+// @downloadURL https://github.com/iMyon/gm_scripts/raw/master/tiebaUploadImagePlus/tiebaUploadImagePlus.user.js
+// @updateURL   https://github.com/iMyon/gm_scripts/raw/master/tiebaUploadImagePlus/tiebaUploadImagePlus.meta.js
 // @icon        http://tb.himg.baidu.com/sys/portrait/item/c339b7e2d3a1b5c4c3a8d726
 // @version     0.0.1
 // ==/UserScript==
